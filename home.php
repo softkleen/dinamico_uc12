f5d2aed29a538a31407a30bab15da6a5e46a8d81
WELLINGTON VIEIRA DOS SANTOS <wellington.vsantos4@senacsp.edu.br>
2019-09-13 19:51:27 -0300

    correção css e inclusão home...

---
+++ home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Dinâmico <?php echo "| Wellington" ?></title>
+</head>
+<body>
+    <div id= "corpo">
+    <h3>:: Posts Recentes</h3>
+        <div id = "ultimo-post-lancado">
+            <small>publicado em 02/03/2017 | 1200 visitas | 300 comentários </small>
+            <a href="index.php?link=" alt=""><img src="img/particulas1.jpg" alt=""></a>
+        </div>
+
+    </div>
+    
+</body>
+</html>
